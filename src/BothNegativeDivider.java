81b5cf01c857c65c8ff820b18e792b3006f5a511
Eytan Anjel <anjel@usc.edu>
2013-09-25 19:40:19 -0700

    tests passing

---
+++ src/BothNegativeDivider.java
@@ -0,0 +1,11 @@
+
+public class BothNegativeDivider implements DivisionStrategy
+{
+
+	@Override
+	public int divide(int dividend, int divisor)
+	{
+		return 2;
+	}
+
+}
--- src/Division.java
+++ src/Division.java
@@ -6,12 +6,16 @@ public class Division
 		if(divisor == 0)
 			return -1;
 		
-		if(dividend < 0)
-			return new NegativeDivider().divide(dividend, divisor);
-		
 		if(dividend >= divisor)
 			return new NormalDivider().divide(dividend, divisor);
 		
+		if(dividend < 0)
+		{
+			if(divisor < 0)
+				return new BothNegativeDivider().divide(dividend, divisor);
+			return new NegativeDivider().divide(dividend, divisor);
+		}
+			
 		return 0;
 	}
 }
